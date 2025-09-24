59481cae75faf614534a91531bfe389eeca1cb94
Alokik Singh naruka <alokiksingh676@gmail.com>
2025-09-24 22:37:29 +0530

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,8 @@
+--CREATE DATABASE → makes a new DB.
+CREATE DATABASE students_db;
+USE students_db;
+--CREATE TABLE → defines columns.
+CREATE TABLE students ( stu_id INT PRIMARY KEY,name VARCHAR(50),
+--PRIMARY KEY → uniquely identifies each row.
+
+--INSERT INTO → adds records.
